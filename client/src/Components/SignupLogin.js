ce2e8b2df10996fa3a723ad6cb3c6b255a315e63
Wakio37 <your_email@example.com>
2024-01-29 19:50:50 +0000

    signup login via API

--- client/src/Components/SignupLogin.js
+++ client/src/Components/SignupLogin.js
@@ -4,12 +4,72 @@ import { Person } from '@mui/icons-material'
 import { LockOutlined } from '@mui/icons-material'
 import { LockOpen } from '@mui/icons-material'
 
+
+
+const Notification = ({status, msg}) => {
+  return (
+    <div style={{color: status === 'success' ? 'green' : 'red', backgroundColor: status === 'success' ? 'lightgreen' : 'lightcoral', padding: '10px', borderRadius: '5px'}}>
+      <div className="text">{status}</div>
+      <div className="text">{msg}</div>
+    </div>
+  )
+}
+
 export const SignupLogin = () => {
 
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  const [firstname, setFirstname] = useState();
+  const [surname, setSurname] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
   const [action, setAction] =useState("Login");
+
+  const [notification, setNotification] = useState({status: '', msg: ''})
+
+  const login = () => {
+    fetch('http://localhost:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password})
+    }) 
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.status === 'error'){
+        setNotification({status: 'error', msg: data.msg})
+      }else{
+        setNotification({status: 'success', msg: data.msg})
+      }
+    })
+
+  }
+  const signup = () => {
+    fetch('http://localhost:5000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({firstname, surname, email, password})
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.status === 'error'){
+        setNotification({status: 'error', msg: data.msg})
+      }else{
+        setNotification({status: 'success', msg: data.msg})
+      }
+    })
+
+
+    
+  }
 
   return (
     <div className='login-container'>
+      {notification.status && <Notification status={notification.status} msg={notification.msg}/>}
       <div className='login-header'>
         <div className='text'>{action}</div>
         <div className='underline'></div>
@@ -17,32 +77,38 @@ export const SignupLogin = () => {
       <div className='inputs'>
         {action==="Login"?<div></div>:<div className='input'>
         <div className='icons'><Person/></div> 
-          <input type ="text" placeholder='first name'/>
+          <input type ="text" placeholder='first name' onChange={(e)=>{setFirstname(e.target.value)}}/>
         </div>}
         {action==="Login"?<div></div>:<div className='input'>
         <div className='icons'><Person/></div>
-          <input type ="text" placeholder='surname'/>
+          <input type ="text" placeholder='surname' onChange={(e)=>{setSurname(e.target.value)}}/>
           </div> }
         
         <div className='input'>
           <div className='icons'><MailOutlined/></div>
-          <input type ="email" placeholder='email'/>
+          <input type ="email" placeholder='email' onChange={(e)=>{setEmail(e.target.value)}}/>
         </div>
         <div className='input'>
         <div className='icons'><LockOutlined/></div>
-          <input type ="password" placeholder='password'/>
+          <input type ="password" placeholder='password' onChange={(e)=>{setPassword(e.target.value)}}/>
         </div>
         {action==="Login"?<div></div>:<div className='input'>
         <div className='icons'><LockOpen/></div>
-          <input type ="password" placeholder='confirm password'/>
+          <input type ="password" placeholder='confirm password' onChange={(e)=>{setConfirmPassword(e.target.value)}}/>
         </div>}
         
         {action==="Sign Up"?<div></div>:<div className='forgot-password'>forgotten password? <span>click here!</span></div>}
         
         <div className='submit-container'>
           <div className={action==="Login"?"submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+
           <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
+
+
         </div>
+        {action==="Sign Up"? <div className={"submit gray"}onClick={signup}>Submit</div>:
+          <div className={"submit gray"}onClick={login}>Submit</div>}
+
       </div>
     </div>
   )
--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, make_response,jsonify,request
 from flask_migrate import Migrate
 from models import db, User
+from flask_cors import CORS
 import jwt
 from utils.sms import SMS
 from utils.mail import Mail
@@ -15,6 +16,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+CORS(app)
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -22,7 +25,7 @@ db.init_app(app)
 @app.route('/signup', methods=['POST'])
 def signup():
     if not request.is_json:
-        return make_response(jsonify({"msg": "Missing JSON in request"}), 400)
+        return make_response(jsonify({"msg": "Missing JSON in request", "status": "error"}), 400)
     
     required_fields = ['firstname', 'surname', 'email', 'password']
     if not all(field in request.json for field in required_fields):
@@ -32,15 +35,15 @@ def signup():
     surname = request.json['surname']
     email = request.json['email']
     password = request.json['password']
-    phone = request.json.get('phone')
+    # phone = request.json.get('phone')
 
-    if phone is None:
-        return make_response(jsonify({"msg": "Phone number is required"}), 400)
+    # if phone is None:
+    #     return make_response(jsonify({"msg": "Phone number is required"}), 400)
 
 
 
     if User.query.filter_by(email=email).first():
-        return make_response(jsonify({"msg": "User already exists"}), 400)
+        return make_response(jsonify({"msg": "User already exists", "status": "error"}), 400)
     
     otp = math.floor(100000 + random.random() * 900000)
 
@@ -51,27 +54,27 @@ def signup():
     db.session.add(user)
     db.session.commit()
 
-    return make_response(jsonify({"msg": "User created successfully"}), 201)
+    return make_response(jsonify({"msg": "User created successfully","status": "success"}), 201)
 
 
 @app.route('/login', methods=['POST'])
 def login():
     if not request.is_json:
-        return make_response(jsonify({"msg": "Missing JSON in request"}), 400)
+        return make_response(jsonify({"msg": "Missing JSON in request", "status": "error"}), 400)
     if 'email' not in request.json or 'password' not in request.json:
-        return make_response(jsonify({"msg": "email and password are required"}), 400)
+        return make_response(jsonify({"msg": "email and password are required", "status": "error"}), 400)
 
     email = request.json['email']
     password = request.json['password']
 
     if email is None or password is None:
-        return make_response(jsonify({"msg": "email and password are required"}), 400)
+        return make_response(jsonify({"msg": "email and password are required", "status": "error"}), 400)
 
     user = User.query.filter_by(email=email, password=password).first()
 
     if not user:
-        return make_response(jsonify({"msg": "Invalid username or password"}), 401)
+        return make_response(jsonify({"msg": "Invalid username or password", "status": "error"}), 401)
 
     token = jwt.encode({'user_id': user.id}, JWT_SECRET, algorithm='HS256')
-    return make_response(jsonify({"msg": "Login successful", "token": token}), 200)
+    return make_response(jsonify({"msg": "Login successful", "token": token, "status": "success"}), 200)
 
