c56145d7f8e63cf371a7f02865df900780a7ec5b
Abigail <abby.a.otieno@gmail.com>
2024-02-06 15:54:39 +0300

    Successfully fetch reviews

--- client/src/Components/Reviews.js
+++ client/src/Components/Reviews.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import {useState, useEffect, React} from 'react'
 
 const Reviews = () => {
+  const [reviewsData, setReviewsData] = useState(null)
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/reviews")
+    .then((response) => response.json())
+    .then((fetchData) => setReviewsData(fetchData))
+  },[])
+
+  console.log(reviewsData)
+  
   return (
     <div>Reviews</div>
   )
